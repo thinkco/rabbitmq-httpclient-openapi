16861610afe9d2f2c19dd5dd8564ec625e939247
Carlos Lozano <carlos@adaptive.me>
2019-05-13 09:51:00 +0200

    Updated generator.

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is the OpenAPI specification of the RabbitMQ HTTP API. 
 
+Use [OpenAPI-Generator](https://github.com/OpenAPITools/openapi-generator) to build client and server stubs. 
+To use ```generator.sh``` included in this project, you must have ```openapi-generator``` command installed on your build environment.
+You can also use the node version, to install: ```npm install @openapitools/openapi-generator-cli -g```.
+
 For more information, please refer to RabbitMQ [HTTP API](https://cdn.rawgit.com/rabbitmq/rabbitmq-management/v3.7.14/priv/www/api/index.html) official documentation.
 
 ---
--- generator.sh
+++ generator.sh
@@ -11,7 +11,7 @@ command -v $GENERATOR >/dev/null 2>&1 || { echo >&2 "I require \"$GENERATOR\" bu
 
 generate_aspnetcore() {
     CONFIG_FILE=aspnetcore.json
-    DESTINATION_DIR=aspnetcore2
+    DESTINATION_DIR=aspnetcore
     DESTINATION_TMP=generated/temp-aspnetcore
     # Remove previously generated files
     rm -f $CONFIG_FILE
@@ -20,7 +20,7 @@ generate_aspnetcore() {
     cat <<EOT >> $CONFIG_FILE
 {
     "packageName" : "$PACKAGE_NETCORE",
-    "packageVersion": "1.0.0",
+    "packageVersion": "1.0.2",
     "targetFramework": "netcoreapp2.0",
     "hideGenerationTimestamp": true,
     "sortParamsByRequiredFlag": true,
@@ -36,16 +36,14 @@ EOT
     openapi-generator generate -i $SOURCE_FILE -g csharp-netcore -o $DESTINATION_DIR -c $CONFIG_FILE
     openapi-generator generate -i $SOURCE_FILE -g openapi-yaml -o $DESTINATION_DIR/docs -c $CONFIG_FILE
     openapi-generator generate -i $SOURCE_FILE -g cwiki -o $DESTINATION_DIR/docs -c $CONFIG_FILE
-    
-    # Customizations to generated artefacts
-    sed -i '' 's#netcoreapp2.1#netcoreapp2.2#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
-    # Compatibility to 2.2
-    sed -i '' 's#CompatibilityVersion.Version_2_1#CompatibilityVersion.Version_2_2#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/Startup.cs
-    # Default JSON serialisation preferences
-    ex -s -c '24i|using Newtonsoft.Json;' -c x $DESTINATION_DIR/src/$PACKAGE_NETCORE/Startup.cs
-    ex -s -c '59i|                    opts.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;' -c x $DESTINATION_DIR/src/$PACKAGE_NETCORE/Startup.cs
-    ex -s -c '60i|                    opts.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;' -c x $DESTINATION_DIR/src/$PACKAGE_NETCORE/Startup.cs
-    ex -s -c '61i|                    opts.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;' -c x $DESTINATION_DIR/src/$PACKAGE_NETCORE/Startup.cs
+
+    # Post Generation Customize
+    sed -i '' 's#<Authors>OpenAPI</Authors>#<Authors>Carlos Lozano Diez</Authors>#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
+    sed -i '' 's#<Company>OpenAPI</Company>#<Company>ThinkCode</Company>#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
+    sed -i '' 's#<AssemblyTitle>OpenAPI Library</AssemblyTitle>#<AssemblyTitle>Rabbitmq HTTP API Client</AssemblyTitle>#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
+    sed -i '' 's#<Copyright>No Copyright</Copyright>#<Copyright>Copyright 2019 Carlos Lozano Diez</Copyright>#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
+    sed -i '' 's#<Description>A library generated from a OpenAPI doc</Description>#<Description>RabbitMQ HTTP API Client for .netCore, tested with v3.7.x of RabbitMQ</Description>#' $DESTINATION_DIR/src/$PACKAGE_NETCORE/$PACKAGE_NETCORE.csproj
+
     # .gitignore
     wget -O $DESTINATION_DIR/.gitignore "https://www.gitignore.io/api/java,maven,macos,aspnetcore,intellij+all,visualstudio,visualstudiocode"
     # Clean-up
